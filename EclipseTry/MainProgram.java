2c545e0edec18a22cb37a69f9f37e762b1044ef8
anwul4 <wulffa@hotmail.com>
2015-09-12 16:16:54 +0200

    wrote the first code

---
+++ EclipseTry/MainProgram.java
@@ -0,0 +1,13 @@
+
+public class MainProgram {
+
+	public int value = 1; 
+	public int secondvalue = 2; 
+	
+	 public static void main(String[] args)
+	{
+		 
+		System.out.println("hey"); 
+	}
+	
+}
